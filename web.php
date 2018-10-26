c96a658f85119d06f2999584276adc967627d1d5
CrazyHappy <crazyhappy@bk.ru>
2018-10-26 18:46:51 +1000

    finish

--- web.php
+++ web.php
@@ -1,7 +1,9 @@
 <?php
 
+//PublicController Описания функций находиться в папке App/Controller а дальше как в Laravel
 
-$router->get('/',           'PublicController@index');
-$router->get('/create',     'PublicController@create');
-$router->get('/pars-date',  'PublicController@parsDate');
-$router->get('/print-txt',  'PublicController@printTxt');
+$router->get('/',           'PublicController@index'); //Главная страница
+$router->get('/create',     'PublicController@create'); //Создания таблицы
+$router->get('/pars-date',  'PublicController@parsDate'); //Чтения JSON
+$router->get('/list-menu',  'PublicController@printTxt'); //list_menu.php выводит в столбец список меню из БД с отсупами
+$router->get('/read',       'PublicController@readFile'); // Чтения и запись в файл
